95c3a1e382f97eb54a962838124c4c7b9c286599
Armen-02 <armen.aws@protonmail.com>
2024-06-06 13:02:12 -0700

    Add Quota back

--- MandrillWebhooksFunction/src/main/java/com/quickfee/lambda/WebhooksHandler.java
+++ MandrillWebhooksFunction/src/main/java/com/quickfee/lambda/WebhooksHandler.java
@@ -14,6 +14,7 @@ import java.util.Map;
  * Mandrill Webhooks Handler for requests to Lambda function.
  */
 public class WebhooksHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         String requestBody = URLDecoder.decode(input.getBody());
         LambdaLogger logger = context.getLogger();
